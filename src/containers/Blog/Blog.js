5df6b486b528d797fca569ec6bc0d4b68b93111b
Iwi4a <ivelin.iliev@hotmail.com>
2018-10-11 22:58:40 +0100

    Lazy load components

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import Posts from '../Posts/Posts';
 import FullPost from '../../components/FullPost/FullPost'
-import NewPost from '../../components/NewPost/NewPost'
+//import NewPost from '../../components/NewPost/NewPost'
 import './Blog.css';
+import asyncComponent from '../../hoc/asyncComponent/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('../../components/NewPost/NewPost')
+});
 
 class Blog extends Component {
-    
+    state = {
+        auth : true,
+    }
     render () {
-        
         return (
             <div className="Blog">
                 <header>
@@ -18,14 +24,16 @@ class Blog extends Component {
                                 <NavLink activeClassName="active-btn" activeStyle={{color:'red'}} to="/home">Home</NavLink>
                             </li>
                             <li>
-                                <NavLink to={{ pathname : '/new-post', hash : '#search' }}>New Post</NavLink>
+                                <NavLink to={{ pathname : '/new-post', }}>New Post</NavLink>
                             </li>
                         </ul>
                     </nav>
                 </header>
                 <Switch>
-                    <Route path='/new-post' component={NewPost} />
+                    { this.state.auth ? <Route path='/new-post' component={AsyncNewPost} /> : null }
                     <Route path='/home' component={Posts} />
+                    <Route render={() => <h1>404 baby</h1>} />
+                
                 </Switch>
             </div>
         );
